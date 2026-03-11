ad2a9f41b2786007c2de34fb67f70075adce93c9
Co-Day <Valentingudko11@gmail.com>
2026-03-11 13:05:16 +0200

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -10,17 +10,12 @@ API_HASH = "dbde6e9d01ba04bcea2f10609054a446"
 # ID твоей группы (ПЗ)
 GROUP_ID = -5214640155
 
-# --- НАСТРОЙКА КЛИЕНТА ЧЕРЕЗ MTPROTO PROXY ---
-# Данные взяты с твоего скриншота (Proxy MTProto)
+# --- НАСТРОЙКА КЛИЕНТА ---
+# Без прокси, так как на Render открытый интернет
 app = Client(
     "my_account", 
     api_id=API_ID, 
-    api_hash=API_HASH,
-    proxy=dict(
-        hostname="194.120.230.106",
-        port=433,  # Если не пойдет, замени на 443
-        secret="3XnnAQIAAQAH8AMDhuJM0t0"
-    )
+    api_hash=API_HASH
 )
 
 # Асинхронный обработчик сообщений
@@ -63,12 +58,12 @@ async def catch_tags(client, message):
         )
         db.commit()
         db.close()
-        print(f"👀 Агент поймал тэг(и) {tags_in_message} и передал Радару!")
+        print(f"👀 Агент поймал тэг(и) {tags_in_message} и передал Радару через базу!")
     except Exception as e:
         print(f"❌ Ошибка базы данных у Агента: {e}")
 
 # Запуск программы
 if __name__ == "__main__":
-    print("🚀 Агент запущен через MTProto Proxy!")
+    print("🚀 Агент запущен на Render!")
     print("📡 Мониторинг группы ПЗ активирован...")
     app.run()
